609cb760b31a740dbc6fb0dbbbfeadf01091eeef
tauraloke <tauraloke@gmail.com>
2024-08-23 16:36:35 +0300

    add railties bootstraps

---
+++ config/initializers/assets.rb
@@ -0,0 +1,7 @@
+# Precompile additional assets.
+# application.js, application.css, and all non-JS/CSS in the app/assets
+# folder are already added.
+
+# frozen_string_literal: true
+
+Rails.application.config.assets.precompile = ['manifest.js']
