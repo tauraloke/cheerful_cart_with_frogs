3f6b05fd45534df4d212107aabfaacb9d3627c25
tauraloke <tauraloke@gmail.com>
2024-08-24 10:48:48 +0300

    fix: class to module

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,7 +4,7 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-class ActiveSupport
+module ActiveSupport
   class TestCase
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
