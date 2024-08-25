38d53f09d325d0c2854ad16a47bd42c726e44c4f
tauraloke <tauraloke@gmail.com>
2024-08-25 11:43:04 +0300

    add meat for a slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,59 @@
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+
+class Reference {
+  constructor(element, updateListener) {
+    this.value = element?.dataset?.value || 0;
+    this.element = element;
+    this.updateListener = updateListener;
+  }
+  update(newValue) {
+    this.onBeforeUpdate(newValue);
+    this.value = newValue;
+    if (this.element) {
+      this.element.dataset.value = newValue;
+      this.element.innerHTML = Reference.prepare(newValue);
+    }
+  }
+  static prepare(value) {
+    return value;
+  }
+  onBeforeUpdate(newValue) {
+    if (typeof this.updateListener == 'function') {
+      this.updateListener(this.value, newValue);
+    }
+  }
+}
+
+class CurrencyReference extends Reference {
+  static prepare(value) {
+    return value.toLocaleString('ru-RU') + ' ₽';
+  }
+}
+
+window.onload = function () {
+  let discountRef = new CurrencyReference(
+    document.querySelector('.cart__total__top__discount'),
+    (_oldValue, newValue) => {
+      totalPriceRef.update(totalSumRef.value - newValue);
+    }
+  );
+
+  let totalPriceRef = new CurrencyReference(
+    document.querySelector('.cart__total__price__value'),
+    (_oldValue, newValue) => {}
+  );
+
+  let totalSumRef = new CurrencyReference(
+    document.querySelector('.cart__total__top__value'),
+    (_oldValue, newValue) => {}
+  );
+
+  document
+    .querySelector('.cart__total__slider')
+    ?.addEventListener('change', (event) => {
+      let newValue = parseInt(event.target?.value);
+      if (newValue !== NaN) discountRef.update(newValue);
+    });
+};
